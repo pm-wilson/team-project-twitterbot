b7437fdeb0ad71b43c6768269e6e877702c09075
Patrick Wilson <pmwilson333@gmail.com>
2020-09-17 15:13:37 -0700

    end point working

--- server.js
+++ server.js
@@ -1,57 +1,29 @@
-const Twit = require('twit');
+const app = require('./lib/app');
+const pool = require('./lib/utils/pool');
 
-const T = new Twit({
-  consumer_key: process.env.APPLICATION_CONSUMER_KEY_HERE,
-  consumer_secret: process.env.APPLICATION_CONSUMER_SECRET_HERE,
-  access_token: process.env.ACCESS_TOKEN_HERE,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET_HERE
+const PORT = process.env.PORT || 7890;
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Started on ${PORT}`);
 });
 
-const count = 100;
+process.on('exit', () => {
+  console.log('Goodbye!');
+  pool.end();
+});
 
-async function getTweet(userName) {
-  const { data } = await T.get('statuses/user_timeline', { screen_name: userName, count });
-  const random = Math.floor(Math.random() * count);
 
-  return data[random].text;
-}
 
-async function tweet(user1, user2) {
 
-  const tweet1 = await getTweet(user1);
-  const tweet2 = await getTweet(user2);
+// require('dotenv').config();
+// //require('./lib/client').connect();
 
-  const firstTweet = tweetProcessor(tweet1);
-  const secondTweet = tweetProcessor(tweet2);
-  const tweetLink = firstTweet.tweetLink ? firstTweet.tweetLink : secondTweet.tweetLink;
+// const app = require('./lib/app');
 
-  console.log(firstTweet.tweetFirstHalf + ' ' + secondTweet.tweetSecondHalf + ' ' + tweetLink);
+// const PORT = process.env.PORT || 3000;
 
-  return firstTweet.tweetFirstHalf + ' ' + secondTweet.tweetSecondHalf + ' ' + tweetLink;
-}
-
-function tweetProcessor(tweet) {
-
-  let tweetWords = tweet.split(' ');
-  let tweetLink = '';
-
-  if(tweetWords[0] === 'RT') {
-    tweetWords = tweetWords.slice(2);
-  }
-
-  if(tweetWords[tweetWords.length - 1].startsWith('http')) {
-    tweetLink = tweetWords.splice(-1);
-  }
-
-  const halfwayPoint = Math.floor(tweetWords.length / 2);
-  const tweetFirstHalf = tweetWords.slice(0, halfwayPoint).join(' ');
-  const tweetSecondHalf = tweetWords.slice(halfwayPoint).join(' ');
-
-  return { tweetFirstHalf, tweetSecondHalf, tweetLink };
-}
-
-async function mashupTweet(user1, user2) {
-  tweet(user1, user2).then((post) => T.post('statuses/update', { status: post }, (err, data) => { console.log('mashupTweet', data.text); }));
-}
-
-setInterval(() => { mashupTweet('mecookiemonster', 'realdonaldtrump'); }, 1000 * 60 * 25);
+// app.listen(PORT, () => {
+//   // eslint-disable-next-line no-console
+//   console.log(`Started on ${PORT}`);
+// });
