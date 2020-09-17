82108540017250da33a3c4f0972262f957ce77c7
Patrick Wilson <pmwilson333@gmail.com>
2020-09-17 16:33:02 -0700

    updated REAL test that works

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,11 @@
-const getTweet = require('../lib/utils/utils')
+const { tweetProcessor } = require('../lib/utils/utils')
 
 describe('twitterbot routes', () => {
   it('should pass', async() => {
-    expect(await getTweet('mecookiemonster')).toEqual(expect.any(String));
+    expect(await tweetProcessor('RT @me here is my cool tweet thing http:thisisalink')).toEqual({
+      tweetFirstHalf: 'here is my', 
+      tweetSecondHalf: 'cool tweet thing', 
+      tweetLink: ['http:thisisalink']
+    });
   });
 });
