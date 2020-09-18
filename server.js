7390fb569a1cc3f112556519712407561d8382b7
Patrick Wilson <pmwilson333@gmail.com>
2020-09-18 14:26:04 -0700

    reset timer to 5 min

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const app = require('./lib/app');
 const pool = require('./lib/utils/pool');
 const { mashupTweet } = require('./lib/utils/utils');
-
-
+const cors = require('cors');
 const PORT = process.env.PORT || 7890;
+app.use(cors);
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Started on ${PORT}`);
 });
 
-setInterval(() => { mashupTweet('mecookiemonster', 'realdonaldtrump'); }, 1000 * 10);
+setInterval(() => { mashupTweet('mecookiemonster', 'realdonaldtrump'); }, 1000 * 60 * 5);
 
 process.on('exit', () => {
   console.log('Goodbye!');
